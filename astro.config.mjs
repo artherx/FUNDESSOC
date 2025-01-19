01d3ff208bf31389eee2eed03cf9aa13823360f5
artherx <xxartherxx@gmail.com>
2025-01-18 20:25:34 -0500

    :chart_with_upwards_trend: main: se a{adio sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,10 @@ import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), mdx()]
+  site: 'https://fundessoc.vercel.app/',
+  integrations: [tailwind(), react(), mdx(), sitemap()]
 });
